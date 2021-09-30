413468897505568e8dd3b47b76c4a3d3f6a7816b
Donna <donna.ismyralda@gmail.com>
2021-09-30 16:32:15 +0800

    Update App.js
    Added comment to quickly navigate between the 2 pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Login from './profile/view-profile/profile';
+import Login from './login/login';
+//import Login from './profile/view-profile/profile';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
