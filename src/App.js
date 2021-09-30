a39518e5c3b532d05b6a640d8355303867e8e930
Donna <donna.ismyralda@gmail.com>
2021-09-30 17:40:03 +0800

    added quick comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-//import Login from './login/login';
-import Login from './profile/view-profile/profile';
+import Login from './login/login';
+//import Login from './profile/view-profile/profile';
+//import Login from './profile/edit-profile/editprofile';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
