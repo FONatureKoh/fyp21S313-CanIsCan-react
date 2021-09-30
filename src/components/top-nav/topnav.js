3f2dc00fada195cba0696ee3080a197a415fb1f1
Donna <donna.ismyralda@gmail.com>
2021-09-30 15:51:46 +0800

    changed function to class
    This will give use easier to do more javascripting inside the class rather than an entire render function.

--- src/components/top-nav/topnav.js
+++ src/components/top-nav/topnav.js
@@ -1,25 +1,29 @@
 import logo from '../../assets/logo.svg';
 import profile from '../../assets/icon-profile.png';
 import './topnav.css';
+import { Component } from 'react';
 
-function topnav() {
-  return (
-    <div className="App">
-        <div className="container">
-            <div className="icon-foc">
-                <img src={logo} className="icon-foc" alt="logo" />
+// the icon-profile should not appear if viewing/editing profile
+// will look into adding the general manager top right icons
+
+class TopNav extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="icon-foc">
+                    <img src={logo} className="icon-foc" alt="logo" />
+                </div>
+                <div className="foodonclick">
+                    <p>Food On Click</p>
+                </div>
+                <div className="icon-profile">
+                    <img src={profile} className="icon-profile" alt="profile" />
+                </div>
             </div>
-            <div className="foodonclick">
-                <p>Food On Click</p>
-            </div>
-            <div className="icon-profile">
-                <img src={profile} className="icon-profile" alt="profile" />
-            </div>
-        </div>
-    </div>
-  );
+        );
+    }
 }
 
-export default topnav;
+export default TopNav;
 
 //<img src="icon-profile.png" alt="foodonclick" width="58" height="51"></img>
--- src/login/login.js
+++ src/login/login.js
@@ -1,26 +1,29 @@
 import logo from '../assets/logo.svg';
 import './login.css';
+import { Component } from 'react';
 
-function Login() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <input type="text" name="username" placeholder="username "/>
-        <input type="password" name="userpw" placeholder="password"/>
-        <select>
-            <option value="customer">Customer</option>
-            <option value="restaurant_admin">Restaurant Manager</option>
-			      <option value="deliveries">Deliveries Manager</option>
-            <option value="reservations">Reservations Manager</option>
-            <option value="administrator">Administrator</option>
-        </select>
-        <button class="go_btn">Log In</button>
-        <div class="whitefont">Don't have an account?</div>
-        <a class="link">Register</a>
-      </header>
-    </div>
-  );
+class Login extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <input type="text" name="username" placeholder="username "/>
+          <input type="password" name="userpw" placeholder="password"/>
+          <select>
+              <option value="customer">Customer</option>
+              <option value="restaurant_admin">Restaurant Manager</option>
+              <option value="deliveries">Deliveries Manager</option>
+              <option value="reservations">Reservations Manager</option>
+              <option value="administrator">Administrator</option>
+          </select>
+          <button class="go_btn">Log In</button>
+          <div class="whitefont">Don't have an account?</div>
+          <a class="link">Register</a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Login;
