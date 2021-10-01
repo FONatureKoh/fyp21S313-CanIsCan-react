c1d940e859962a0692de64464a9cbad4f7be1db4
Catus1234 <89518980+Catus1234@users.noreply.github.com>
2021-10-01 22:50:34 +0800

    Update generalmanager.js
    updated generalmanager.js to functional component.

--- src/restaurant/landingpage/generalmanager.js
+++ src/restaurant/landingpage/generalmanager.js
@@ -2,23 +2,21 @@ import TopNav from '../../components/top-nav/topnav';
 import './generalmanager.css';
 import { Component } from 'react';
 
-class GeneralManagerView extends Component{
-    render(){
-        return (
-            <div className="main">
-                <TopNav />
-                <div className="container">
-                    <div className="buttonContainer">
-                        <div className="buttons">
-                            <button className="menu_btn">Manage Menu Items</button>
-                            <button className="menu_btn">Manage Employee Profiles</button>
-                            <button className="menu_btn">View Store Analytics</button>
-                        </div>
-                    </div>
+import React from 'react'
+
+export default function generalmanager() {
+    return (
+        <div className="main">
+        <TopNav />
+        <div className="container">
+            <div className="buttonContainer">
+                <div className="buttons">
+                    <button className="menu_btn">Manage Menu Items</button>
+                    <button className="menu_btn">Manage Employee Profiles</button>
+                    <button className="menu_btn">View Store Analytics</button>
                 </div>
             </div>
-        );
-    }
+        </div>
+    </div>
+    )
 }
-
-export default GeneralManagerView;
