67f79069b1d9d3f345844d9a8cc364d7fed62eb5
Donna <donna.ismyralda@gmail.com>
2021-10-02 10:50:41 +0800

    Revert "Merge branch 'main' of https://github.com/FONatureKoh/fyp21S313-CanIsCan-react into main"
    This reverts commit 22a83dca0995db17b1cc8ab17f512d51390dbb1f, reversing
    changes made to 5cdd6f221634a9eff66ec4eff27982a817a706fd.

--- src/restaurant/editmenu/EditMenu.js
+++ src/restaurant/editmenu/EditMenu.js
@@ -1,11 +1,8 @@
-import React, {useRef, useState}from 'react'
+import React from 'react'
 import TopNav from '../../components/top-nav/topnav'
 import './EditMenu.css'
 
 export default function EditMenu() {
-
-  const dynamic = useRef();
-  const [loaded, setLoaded] = useState(false);
 
   const data = [
     {
@@ -35,27 +32,20 @@ export default function EditMenu() {
     {
       name: 'Tian',
       age: 312
-    },
-    {
-      name: 'Tian',
-      age: 312
-    },
-    {
-      name: 'Tian',
-      age: 312
     }
   ]
 
   return (
     <div className="main">
         <TopNav />
-        <div className="add">
-            <button className="addBtn">Add Item</button>
-        </div>
-        <div className="buttons" ref= {dynamic}>
-          {
-            data.map(element =>  <button class="menu_btn"> {element.name} </button>)
-          }
+        <div className="container">
+          <div className="buttonContainer">
+            <div className="buttons">
+              {
+                data.map(element => <button class="menu_btn"> {element.name} </button>) 
+              }
+            </div>
+          </div>
         </div>
     </div>
   )
