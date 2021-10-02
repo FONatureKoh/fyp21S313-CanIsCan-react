5cdd6f221634a9eff66ec4eff27982a817a706fd
Donna <donna.ismyralda@gmail.com>
2021-10-02 10:50:21 +0800

    update topnav

--- src/components/top-nav/topnav.js
+++ src/components/top-nav/topnav.js
@@ -1,29 +1,23 @@
 import logo from '../../assets/logo.svg';
 import profile from '../../assets/icon-profile.png';
 import './topnav.css';
-import { Component } from 'react';
 
 // the icon-profile should not appear if viewing/editing profile
 // will look into adding the general manager top right icons
 
-class TopNav extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="icon-foc">
-                    <img src={logo} className="icon-foc" alt="logo" />
-                </div>
-                <div className="foodonclick">
-                    <p>Food On Click</p>
-                </div>
-                <div className="icon-profile">
-                    <img src={profile} className="icon-profile" alt="profile" />
-                </div>
+export default function TopNav(){
+    return (
+        <div className="holder">
+            <div className="icon-foc">
+                <img src={logo} className="icon-foc" alt="logo" />
             </div>
-        );
-    }
+            <div className="foodonclick">
+                <p>Food On Click</p>
+            </div>
+            <div className="icon-profile">
+                <img src={profile} className="icon-profile" alt="profile" />
+            </div>
+        </div>
+    );
 }
-
-export default TopNav;
-
 //<img src="icon-profile.png" alt="foodonclick" width="58" height="51"></img>
