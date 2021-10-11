0493ac1a8744951d3cc5292c5a675c515af69284
Donna <donna.ismyralda@gmail.com>
2021-10-11 15:49:09 +0800

    added route to edit info

--- src/restaurant/GeneralManager/components/restaurantprofile.js
+++ src/restaurant/GeneralManager/components/restaurantprofile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Grid, Button, Typography, Switch, Divider, CardContent, CardHeader, Card } from '@mui/material'
 import bannerpic from '../../../assets/temp/eg-biz1.png'
-import { Route, Switch as Switch2 } from 'react-router-dom'
+import { Route, Link, Switch as Switch2 } from 'react-router-dom'
 import EditInformation from './editinformation'
 
 export default function ViewInfo({isChecked, toggleChecked}) {
@@ -21,50 +21,49 @@ export default function ViewInfo({isChecked, toggleChecked}) {
 
   return (
     <div>
-        <Switch2>
-            <Route exact path="/generalmanager/restaurantinformation">
-                <Card variant="outlined" sx={{padding:'5px', borderRadius:'10px'}}>
-                    <CardHeader title="Restaurant Information" />
-                    <CardContent >
-                        <Grid container sx={{margin:'auto', textAlign:'left', width: '70%'}} >
-                            <Grid item xs={12} sx={{textAlign:'center'}}>
-                                <img src={bannerpic} width="60%" alt="banner"/>
-                                <Typography sx={{textAlign:'center', fontSize:'1 0px', textDecoration:'underline', cursor:'pointer'}}>Upload Photo</Typography>
-                            </Grid>
+      <Switch2>
+        <Route exact path="/generalmanager/restaurantinformation">
+            <Card variant="outlined" sx={{padding:'5px', borderRadius:'10px'}}>
+              <CardHeader title="Restaurant Information" />
+              <CardContent >
+                  <Grid container sx={{margin:'auto', textAlign:'left', width: '70%'}} >
+                      <Grid item xs={12} sx={{textAlign:'center'}}>
+                          <img src={bannerpic} width="60%" alt="banner"/>
+                          <Typography sx={{textAlign:'center', fontSize:'1 0px', textDecoration:'underline', cursor:'pointer'}}>Upload Photo</Typography>
+                      </Grid>
 
-                        <Grid item xs={12} sx={{textAlign:'center', marginTop:'2%', marginBottom:'2%'}}>
-                            <Divider />
-                            <Typography sx={{marginTop:'20px', marginBottom:'20px'}}>Open For Business <Switch checked={isChecked} size="large" onClick={()=>toggleChecked()}/></Typography>
-                            <Divider />
-                        </Grid>
+                  <Grid item xs={12} sx={{textAlign:'center', marginTop:'2%', marginBottom:'2%'}}>
+                      <Divider />
+                      <Typography sx={{marginTop:'20px', marginBottom:'20px'}}>Open For Business <Switch checked={isChecked} size="large" onClick={()=>toggleChecked()}/></Typography>
+                      <Divider />
+                  </Grid>
+                  
+                  <Grid item xs={12} sx={{textAlign:'center'}}>
+                      <Typography sx={boldtitle}>Restaurant Name</Typography>
+                      <Typography>
+                          {restaurantinfo.name}
+                      </Typography>
 
-            
-                        <Grid item xs={12} sx={{textAlign:'center'}}>
-                            <Typography sx={boldtitle}>Restaurant Name</Typography>
-                            <Typography>
-                                {restaurantinfo.name}
-                            </Typography>
+                      <Typography sx={boldtitle}>Restaurant Contact Number</Typography>
+                      <Typography>
+                          {restaurantinfo.phone}
+                      </Typography>
 
-                            <Typography sx={boldtitle}>Restaurant Contact Number</Typography>
-                            <Typography>
-                                {restaurantinfo.phone}
-                            </Typography>
-
-                            <Typography sx={boldtitle}>Restaurant Address</Typography>
-                            <Typography>
-                                {restaurantinfo.address}
-                            </Typography>
-                        </Grid>
-            
-                        <Grid item xs={12} sx={{textAlign:'center', marginTop:'5%'}}>
-                            <Button variant="contained" color="inherit" sx={{width:'45%', bgcolor:"#969696", textAlign:'flex-start'}}>Edit Information</Button>
-                        </Grid>
-                    </Grid>
-                </CardContent>
-                </Card>
-            </Route>
-            <Route path="/generalmanager/restaurantinformation/edit" component= {EditInformation}/>
-        </Switch2>
+                      <Typography sx={boldtitle}>Restaurant Address</Typography>
+                      <Typography>
+                          {restaurantinfo.address}
+                      </Typography>
+                  </Grid>
+      
+                  <Grid item xs={12} sx={{textAlign:'center', marginTop:'5%'}}>
+                      <Button variant="contained" color="inherit"  component={Link} to="/generalmanager/restaurantinformation/edit" sx={{width:'45%', bgcolor:"#969696", textAlign:'flex-start'}}>Edit Information</Button>
+                  </Grid>
+                  </Grid>
+              </CardContent>
+            </Card>
+        </Route>
+        <Route path="/generalmanager/restaurantinformation/edit" component= {EditInformation}/>
+      </Switch2>
     </div>
   )
 }
