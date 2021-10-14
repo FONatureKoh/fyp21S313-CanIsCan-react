3c607d4145eb3d85e4141c897d8a1c3979dbae5a
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-14 12:57:28 +0800

    Profile Controller deployed

---
+++ src/profile/profile_controller.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+/*****************************************************************************************
+ * User retrieving profile information based on the usertype                             *
+ * ***************************************************************************************
+ * - This takes all the info from the RGM and creates the subuser based on the given data
+ * ***************************************************************************************/
+export function retrieveUserProfile() {
+  // Axios request config to be declared first
+  const axiosConfig = {
+    headers: {'Authorisation': window.sessionStorage.accessToken}
+  };
+
+  console.log(axiosConfig);
+
+  return axios.get(`https://api.cancanfoodapp.xyz/users/profilemanagement`, axiosConfig)
+    .then(res => {
+      // In here we can choose what we want to do with the response of the request
+      // console.log(res)
+      console.log(res.data);
+      return res.data;
+    })
+    .catch(err => {
+      console.log(err)
+    });
+};
