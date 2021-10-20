3ebd27eb1b9d5f223bcac1a538349615d6608c73
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-20 20:06:41 +0800

    Register Controllers uploaded

--- src/register/register_controller.js
+++ src/register/register_controller.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 ******************************************************************************************/
 const config = require('../store/config.json');
 
+/*****************************************************************************************
+ * Restaurant Registration controller                                                    *
+******************************************************************************************/
 export function restaurantRegister (username, restaurat_name, email, phone) {
   // x-www-form-urlencoded form creation
   const params = new URLSearchParams();
@@ -21,6 +24,34 @@ export function restaurantRegister (username, restaurat_name, email, phone) {
   };
 
   return axios.post(`${config.apiDomain}/register/restaurant`, params, axiosConfig)
+    .then(res => {
+      // In here we can choose what we want to do with the response of the request
+      // console.log(res)
+      return res.data;
+    })
+    .catch(err => {
+      console.log(err)
+    });
+}
+
+/*****************************************************************************************
+ * Customer Registration controller                                                      *
+******************************************************************************************/
+export function customerRegister (username, password, email) {
+  // x-www-form-urlencoded form creation
+  const params = new URLSearchParams();
+  params.append('username', username);
+  params.append('password', password);
+  params.append('email', email);
+
+  // Looks like have to config so that the request becomes a form request
+  const axiosConfig = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+
+  return axios.post(`${config.apiDomain}/register/customer`, params, axiosConfig)
     .then(res => {
       // In here we can choose what we want to do with the response of the request
       // console.log(res)
