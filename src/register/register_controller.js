1ccffe7082953de86415efeb270983f4de5a3c6c
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-20 10:49:14 +0800

    Create register_controller.js

---
+++ src/register/register_controller.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+/*****************************************************************************************
+ * Some application settings to be used throughout the controller
+******************************************************************************************/
+const config = require('../store/config.json');
+
+export function restaurantRegister () {
+  
+}
