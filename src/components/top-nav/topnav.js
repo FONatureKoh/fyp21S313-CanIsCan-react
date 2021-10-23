60c339cd2edec7e49e78df942c191613c3707c01
FONatureKoh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-23 13:31:21 +0800

    Updated indentation

--- src/components/top-nav/topnav.js
+++ src/components/top-nav/topnav.js
@@ -17,64 +17,64 @@ import { Divider, Button } from '@mui/material';
 // will look into adding the general manager top right icons
 
 export default function TopNav(){
-    const history = useHistory();
+  const history = useHistory();
 
-    /*Menu*/
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-    
-    function viewprofile(){
-      let path = "/profile";
-      history.push(path);
-    }
+  /*Menu*/
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  function viewprofile(){
+    let path = "/profile";
+    history.push(path);
+  }
 
-    return (
-      <div className="holder">
-        <div className="icon-foc">
-          <img src={logo} className="icon-foc" alt="logo" />
-        </div>
-        <div className="foodonclick">
-          <p>Food On Click</p>
-        </div>
-        <div className="icon-profile">
-        <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }} >
-          <Avatar src={profilepic} className="icon-profile" alt="profile" onClick= {viewprofile} sx={{ width: 60 , height: 60}}/>
-        </IconButton>
-        </div>
-        <Menu
-          anchorEl={anchorEl}
-          id="menu"
-          open={open}
-          onClose={handleClose}
-          onClick={handleClose}
-          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-          PaperProps={{
-            elevation: 0,
-            sx: {
-              overflow: 'visible',
-              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-              mt: 1.5,}}}
-        >
-          <MenuItem onClick={handleClose}> <PersonIcon color="action" fontSize="small" sx={{mr: 1}}/> Edit Profile</MenuItem>
-          <MenuItem onClick={handleClose}> <LockIcon color="action" fontSize="small" sx={{mr: 1}}/>Change Password</MenuItem>
-          <MenuItem onClick={handleClose}> <Settings color="action" fontSize="small" sx={{mr: 1}}/> Settings</MenuItem>
-          <Divider />
-          <MenuItem onClick={handleClose}>
-            <Button variant="outlined" color="inherit" sx={{width:"100%"}}>Logout</Button>
-          </MenuItem>
-        </Menu>
-        {/*
-        
-        <img src={profile} className="icon-profile" alt="profile" onClick= {viewprofile}/>
-        
-        */}    
+  return (
+    <div className="holder">
+      <div className="icon-foc">
+        <img src={logo} className="icon-foc" alt="logo" />
       </div>
-    );
+      <div className="foodonclick">
+        <p>Food On Click</p>
+      </div>
+      <div className="icon-profile">
+      <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }} >
+        <Avatar src={profilepic} className="icon-profile" alt="profile" onClick= {viewprofile} sx={{ width: 60 , height: 60}}/>
+      </IconButton>
+      </div>
+      <Menu
+        anchorEl={anchorEl}
+        id="menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,}}}
+      >
+        <MenuItem onClick={handleClose}> <PersonIcon color="action" fontSize="small" sx={{mr: 1}}/> Edit Profile</MenuItem>
+        <MenuItem onClick={handleClose}> <LockIcon color="action" fontSize="small" sx={{mr: 1}}/>Change Password</MenuItem>
+        <MenuItem onClick={handleClose}> <Settings color="action" fontSize="small" sx={{mr: 1}}/> Settings</MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <Button variant="outlined" color="inherit" sx={{width:"100%"}}>Logout</Button>
+        </MenuItem>
+      </Menu>
+      {/*
+      
+      <img src={profile} className="icon-profile" alt="profile" onClick= {viewprofile}/>
+      
+      */}    
+    </div>
+  );
 }
