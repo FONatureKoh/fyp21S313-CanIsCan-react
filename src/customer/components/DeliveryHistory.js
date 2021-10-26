f5b760f9524e2855c28cc91ecf11483966f4ba42
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-26 23:10:47 +0800

    Update DeliveryHistory.js

--- src/customer/components/DeliveryHistory.js
+++ src/customer/components/DeliveryHistory.js
@@ -57,6 +57,7 @@ export default function DeliveryHistory() {
         orderDetailsArray.push(tempJSON);
         console.log(orderDetailsArray);
       })
+      return orderDetailsArray;
     }
     catch (error) {
       return error;
