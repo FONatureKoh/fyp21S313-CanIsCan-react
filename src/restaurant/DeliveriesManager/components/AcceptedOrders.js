cf88bc9c72e467077bc6008637196936306ce1f6
Duncan2244 <91479045+Duncan2244@users.noreply.github.com>
2021-10-27 14:17:46 +0800

    Deliveries Manager 2

--- src/restaurant/DeliveriesManager/components/AcceptedOrders.js
+++ src/restaurant/DeliveriesManager/components/AcceptedOrders.js
@@ -1,15 +1,152 @@
-import React from 'react'
-import { Card, CardHeader, CardContent, Box } from '@mui/material'
+import React, { useState } from 'react'
+import { Card, CardHeader, CardContent, Box, Typography, Stepper, Step, StepLabel, Divider, Accordion, AccordionSummary, AccordionDetails, Grid, ListItem, Button } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+const themes = {
+  textHeader: {
+    fontSize:'1 0px', 
+    fontWeight:'bold', 
+    mt: '20px',
+    mb: '10px'
+  }
+};
 
 export default function AcceptedOrders() {
+
+  // ACCORDION CONTROL
+const [accOpen, setAccOpen] = useState(false);
+
+//CART TESTING
+const [realCart, setRealCart]= useState([
+  {
+    id: 1,
+    item: 'dog food',
+    price: 12.1,
+    qty: 3 
+  },
+  {
+    id: 2,
+    item: 'cat food',
+    price: 13,
+    qty: 2
+  },
+  {
+    id: 3,
+    item: 'giraffe food',
+    price: 23,
+    qty: 1
+  },
+  {
+    id: 4,
+    item: 'giraffe food',
+    price: 23,
+    qty: 1
+  },
+  {
+    id: 5,
+    item: 'giraffe food',
+    price: 23,
+    qty: 1
+  }
+])
+
   return (
-      <Card variant="outlined" sx={{padding:'5px', borderRadius:'10px'}}>
-        <CardHeader title="View Accepted Orders" />
+    <Card variant="outlined" sx={{padding:'5px', borderRadius:'10px'}}>
+    <CardHeader title="Pending Orders" />
+    <CardContent>
+      <>
+      <Card variant="outlined" sx={{padding:'5px', borderRadius:'10px', width:'80%', margin:'0px auto'}}>
         <CardContent >
-          <Box > 
-             
+          <Box width="100%" sx={{margin:'0px auto', textAlign:"center"}}>
+            <Accordion sx={{border:'1px solid #eeeeee', mt:'20px'}} expanded={accOpen} >
+              {/* HEADER OF ACCORDION */}
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="item-details"
+                id="item-details"
+                sx={{borderBottom:'0.5px solid #eeeeee'}}
+                onClick={()=>{setAccOpen(!accOpen)}}
+              >
+                <Typography sx={{fontSize:'1 0px', fontWeight:'bold', }}>
+                  Order #333333
+                </Typography>
+              </AccordionSummary>
+              {/* INNDER ACCORDION */}
+              <AccordionDetails>
+                {realCart.map(item => (
+                  <ListItem key={item.id} sx={{margin:'20px auto'}}>
+                    <Box width='70%'>
+                      <Typography variant="h6">
+                        {item.item}
+                      </Typography>
+                      <Typography variant="subtitle">
+                        Unit Price: S${item.price.toFixed(2)}
+                      </Typography>
+                    </Box>
+                    <Box width='30%' textAlign='right' sx={{mt:'10px'}}>
+                      <Typography variant="subtitle2">
+                        Quantity: {item.qty}
+                      </Typography>
+                        <Typography variant="subtitle2">
+                          Price: S$ {(item.qty * item.price).toFixed(2)}
+                          
+                        </Typography>
+                      </Box>
+                  </ListItem>
+                ))}
+                <Box m={1} pt={1}>
+                <Button variant="outlined" id="1" color="inherit" fullWidth>Completed
+                </Button>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion sx={{border:'1px solid #eeeeee', mt:'20px'}} expanded={accOpen} >
+              {/* HEADER OF ACCORDION */}
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="item-details"
+                id="item-details"
+                sx={{borderBottom:'0.5px solid #eeeeee'}}
+                onClick={()=>{setAccOpen(!accOpen)}}
+              >
+                <Typography sx={{fontSize:'1 0px', fontWeight:'bold', }}>
+                  Order #555555
+                </Typography>
+              </AccordionSummary>
+              {/* INNDER ACCORDION */}
+              <AccordionDetails>
+                {realCart.map(item => (
+                  <ListItem key={item.id} sx={{margin:'20px auto'}}>
+                    <Box width='70%'>
+                      <Typography variant="h6">
+                        {item.item}
+                      </Typography>
+                      <Typography variant="subtitle">
+                        Unit Price: S${item.price.toFixed(2)}
+                      </Typography>
+                    </Box>
+                    <Box width='30%' textAlign='right' sx={{mt:'10px'}}>
+                      <Typography variant="subtitle2">
+                        Quantity: {item.qty}
+                      </Typography>
+                        <Typography variant="subtitle2">
+                          Price: S$ {(item.qty * item.price).toFixed(2)}
+                        </Typography>
+                      </Box>
+                  </ListItem>
+                ))}
+                <Box m={1} pt={1}>
+                <Button variant="outlined" id="2" color="inherit" fullWidth>Completed
+                </Button>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
           </Box>
         </CardContent>
       </Card>
-  )
+        </>
+    </CardContent>
+  </Card>
+)
 }
