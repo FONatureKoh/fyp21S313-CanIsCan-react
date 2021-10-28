1552785126f97d759d7bef6f3972eb4cdc4c4245
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-28 23:21:30 +0800

    Updated Deliveries Manager matters

--- src/restaurant/DeliveriesManager/components/DelAccordion.js
+++ src/restaurant/DeliveriesManager/components/DelAccordion.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react'
 import { Card, CardHeader, CardContent, Box, Typography, Stepper, Step, StepLabel, Divider, Accordion, AccordionSummary, AccordionDetails, Grid, ListItem, Button } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { updateDOStatus } from '../dm_controller';
 
 export default function DelAccordion({item}) {
   
   // ACCORDION CONTROL
   const [accOpen, setAccOpen] = useState(false);
   const [innerAccOpen, setInnerAccOpen] = useState(false);
+
+  // Button functions
+  const setCancelled = () => {
+    updateDOStatus(item.orderID, "Cancelled")
+      .then((response) => {
+        alert(response.api_msg);
+      })
+  }
+
+  const setPreparing = () => {
+    updateDOStatus(item.orderID, "Preparing")
+      .then((response) => {
+        alert(response.api_msg);
+      })
+  }
+
+  const setDelivering = () => {
+    updateDOStatus(item.orderID, "Delivering")
+      .then((response) => {
+        alert(response.api_msg);
+      })
+  }
 
   return (
     <Accordion sx={{border:'1px solid #eeeeee', mt:'20px'}} expanded={accOpen} >
@@ -105,15 +128,15 @@ export default function DelAccordion({item}) {
         {item.status === 'Pending' ? (
         <>
           <Box m={1} pt={5}>
-            < Button variant="outlined" id="1" color="inherit" fullWidth>Accept </Button>
+            < Button onClick={setPreparing} variant="outlined" id="1" color="inherit" fullWidth>Accept </Button>
           </Box>
           <Box m={1} pt={1}>
-            <Button variant="outlined" id="1" color="error" fullWidth>Decline </Button>
+            <Button onClick={setCancelled} variant="outlined" id="1" color="error" fullWidth>Decline </Button>
           </Box>
         </>) : item.status === 'Preparing' ? (
         <>
           <Box m={1} pt={5}>
-            < Button variant="outlined" id="1" color="inherit" fullWidth>Out for delivery </Button>
+            < Button onClick={setDelivering} variant="outlined" id="1" color="inherit" fullWidth>Out for delivery </Button>
           </Box>
         </>) : (<></>)}
 
